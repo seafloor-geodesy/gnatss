8fb54ce82ba781c2118c280d744e72e2b235bbac
Landung 'Don' Setiawan <landungs@uw.edu>
2023-04-06 16:05:38 -0700

    Add initial CLI help test

---
+++ tests/test_cli.py
@@ -0,0 +1,34 @@
+from typer.testing import CliRunner
+import pytest
+
+from seagap.cli import app
+
+runner = CliRunner()
+
+@pytest.mark.parametrize(
+    "commands,expected_stdouts",
+    [
+        (
+            [],
+            [
+                "GNSS-A Processing in Python",
+                "run    Run the full pre-processing routine for GNSS-A"
+            ]
+        ),
+        (
+            ["run"],
+            [
+                "Run the full pre-processing routine for GNSS-A",
+                "--config-yaml",
+                "Custom path to configuration yaml file."
+            ]
+        )
+    ],
+    ids=["root", "run"]
+)
+def test_app_help(commands, expected_stdouts):
+    result = runner.invoke(app, commands + ["--help"])
+    assert result.exit_code == 0
+
+    for expected in expected_stdouts:
+        assert expected in result.stdout
