ff86c7fbdb48ec7dc970f35eccf408fe3ff96768
Landung 'Don' Setiawan <landungs@uw.edu>
2023-04-26 12:13:10 -0700

    Update geo module and add tests

--- src/seagap/utilities/geo.py
+++ src/seagap/utilities/geo.py
@@ -4,17 +4,15 @@ Geospatial utilities module
 """
 from typing import Tuple
 
+import numpy as np
 import pyproj
 
 ECEF_PROJ = pyproj.Proj(proj="geocent", ellps="WGS84")  # Earth Centered Fixed (x, y, z)
 LLA_PROJ = pyproj.Proj(
     proj="longlat", ellps="WGS84"
 )  # Longitude Latitude (lon, lat, alt)
-
-
-def _get_topo_proj(lon, lat, alt) -> pyproj.Proj:
-    """Get topocentric Proj object"""
-    return pyproj.Proj(proj="topocentric", ellps="WGS84", lat_0=lat, lon_0=lon, h_0=alt)
+GEODETIC_PRECISION = 9  # 9 decimal points
+GEOCENTRIC_PRECISION = 3  # 3 decimal points
 
 
 def geocentric2geodetic(x: float, y: float, z: float) -> Tuple[float, float, float]:
@@ -37,7 +35,8 @@ def geocentric2geodetic(x: float, y: float, z: float) -> Tuple[float, float, flo
         The lon, lat, alt coordinates in degrees
     """
     transformer = pyproj.Transformer.from_proj(ECEF_PROJ, LLA_PROJ)
-    return transformer.transform(x, y, z, radians=False)
+    coordinates = transformer.transform(x, y, z, radians=False)
+    return np.round(coordinates, GEODETIC_PRECISION)
 
 
 def geodetic2geocentric(
@@ -62,62 +61,78 @@ def geodetic2geocentric(
         The x, y, z coordinates in meters
     """
     transformer = pyproj.Transformer.from_proj(LLA_PROJ, ECEF_PROJ)
-    return transformer.transform(lon, lat, alt, radians=False)
+    coordinates = transformer.transform(lon, lat, alt, radians=False)
+    return np.round(coordinates, GEOCENTRIC_PRECISION)
 
 
-def geodetic2enu(
-    lon: float, lat: float, alt: float, lon_org: float, lat_org: float, alt_org: float
-) -> Tuple[float, float, float]:
+def __get_rotation_matrix(
+    lat_org: float, lon_org: float, to_enu: bool = True
+) -> np.ndarray:
+    """Helper function for ECEF to ENU and vice versa"""
+    # Setup
+    cos_lat = np.cos(lat_org)
+    sin_lat = np.sin(lat_org)
+    cos_lon = np.cos(lon_org)
+    sin_lon = np.sin(lon_org)
+
+    if to_enu:
+        return np.array(
+            [
+                [-sin_lon, cos_lon, 0],
+                [-sin_lat * cos_lon, -sin_lat * sin_lon, cos_lat],
+                [cos_lat * cos_lon, cos_lat * sin_lon, sin_lat],
+            ]
+        )
+    return np.array(
+        [
+            [-sin_lon, -sin_lat * cos_lon, cos_lat * cos_lon],
+            [cos_lon, -sin_lat * sin_lon, cos_lat * sin_lon],
+            [0, cos_lat, sin_lat],
+        ]
+    )
+
+
+def geocentric2enu(x, y, z, lon_org, lat_org, alt_org):
     """
-    Convert Geodetic coordinates (lon,lat,alt) to Topocentric coordinates (east,north,up)
-    based on a specific topocentric origin described as geographic coordinates.
+    Transform Geocentric coordinate (x,y,z) to a local ENU coordinate(east, north, up)
+    based on a reference point in Geodetic coordinate (lon, lat, alt)
 
-    See https://proj.org/operations/conversions/topocentric.html for more details.
+    See https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#From_ECEF_to_ENU
 
     Parameters
     ----------
-    lon : float
-        Longitude in degrees
-    lat : float
-        Latitude in degrees
-    alt : float
-        Altitude in degrees
+    x : float
+        Geocentric x in meters
+    y : float
+        Geocentric y in meters
+    z : float
+        Geocentric z in meters
     lon_org : float
-        Topocentric origin (x) as longitude in degrees
+        Reference origin longitude in degrees
     lat_org : float
-        Topocentric origin (y) as latitude in degrees
+        Reference origin latitude in degrees
     alt_org : float
-        Topocentric origin (z) as altitude in degrees
+        Reference origin altitude in degrees
 
     Returns
     -------
     east, north, up
         The east, north, up coordinates in meters
     """
+    origin_xyz = geodetic2geocentric(lon_org, lat_org, alt_org)
+    delta_xyz = np.column_stack([np.array([x, y, z]) - origin_xyz])  # D(3, 1)
 
-    # Create topocentric proj obj based on specified origin
-    topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
-
-    # Create the projection transform pipeline string
-    # This pipeline is for lonlatalt -> xyz -> enu
-    projs = ["+proj=pipeline", ECEF_PROJ, topo_proj]
-    pyproj_pipe = [proj.srs if not isinstance(proj, str) else proj for proj in projs]
-    pyproj_pipeline = " +step ".join(pyproj_pipe)
-
-    # Get transformer from pipeline string
-    transformer = pyproj.Transformer.from_pipeline(pyproj_pipeline)
-
-    return transformer.transform(lon, lat, alt, radians=False)
+    # Rotation matrix R(3, 3)
+    R = __get_rotation_matrix(lat_org, lon_org)
+    return np.dot(R, delta_xyz)  # E(3, 1)
 
 
-def enu2geodetic(
-    east: float, north: float, up: float, lon_org: float, lat_org: float, alt_org: float
-) -> Tuple[float, float, float]:
+def enu2geocentric(e, n, u, lat_org, lon_org, alt_org):
     """
-    Convert Topocentric coordinates (east,north,up) to Geodetic coordinates (lon,lat,alt)
-    based on a specific topocentric origin described as geographic coordinates.
+    Transform local ENU coordinate(east, north, up) to Geocentric coordinate (x,y,z)
+    based on a reference point in Geodetic coordinate (lon, lat, alt)
 
-    See https://proj.org/operations/conversions/topocentric.html for more details.
+    See https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#From_ENU_to_ECEF
 
     Parameters
     ----------
@@ -128,29 +143,121 @@ def enu2geodetic(
     up : float
         Up in meters
     lon_org : float
-        Topocentric origin (x) as longitude in degrees
+        Reference origin longitude in degrees
     lat_org : float
-        Topocentric origin (y) as latitude in degrees
+        Reference origin latitude in degrees
     alt_org : float
-        Topocentric origin (z) as altitude in degrees
+        Reference origin altitude in degrees
 
     Returns
     -------
-    longitude, latitude, altitude
-        The lon, lat, alt coordinates in degrees
+    x, y, z
+        The x, y, z coordinates in meters
     """
+    origin_xyz = np.column_stack(
+        [geodetic2geocentric(lon_org, lat_org, alt_org)]
+    )  # O(3, 1)
+    enu = np.column_stack([np.array([e, n, u])])  # E(3, 1)
+    # Rotation matrix R(3, 3)
+    R = __get_rotation_matrix(lat_org, lon_org, to_enu=False)
+    return np.dot(R, enu) + origin_xyz  # X(3, 1)
 
-    # Create topocentric proj obj based on specified origin
-    topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
 
-    # Create projection transform pipeline string
-    # This pipeline is for enu -> xyz
-    pyproj_pipeline = f"+proj=pipeline +inv +step {topo_proj.srs}"
+# 04/25/2023 Don Setiawan
+# Code below uses Proj... but it provides different result
+# need to investigate as to what's going on here and its approach compared to the fortran
+# The Proj method does take into account the ellipsoid
+#
+# def _get_topo_proj(lon, lat, alt) -> pyproj.Proj:
+#     """Get topocentric Proj object"""
+#     return pyproj.Proj(proj="topocentric", ellps="WGS84", lat_0=lat, lon_0=lon, h_0=alt)
 
-    # Get transformer from pipeline string
-    transformer1 = pyproj.Transformer.from_pipeline(pyproj_pipeline)
+# def geodetic2enu(
+#     lon: float, lat: float, alt: float, lon_org: float, lat_org: float, alt_org: float
+# ) -> Tuple[float, float, float]:
+#     """
+#     Convert Geodetic coordinates (lon,lat,alt) to Topocentric coordinates (east,north,up)
+#     based on a specific topocentric origin described as geographic coordinates.
 
-    x, y, z = transformer1.transform(east, north, up, radians=False)
+#     See https://proj.org/operations/conversions/topocentric.html for more details.
 
-    # Return lonlatalt by converting xyz -> lonlatalt
-    return geocentric2geodetic(x, y, z)
+#     Parameters
+#     ----------
+#     lon : float
+#         Longitude in degrees
+#     lat : float
+#         Latitude in degrees
+#     alt : float
+#         Altitude in degrees
+#     lon_org : float
+#         Topocentric origin (x) as longitude in degrees
+#     lat_org : float
+#         Topocentric origin (y) as latitude in degrees
+#     alt_org : float
+#         Topocentric origin (z) as altitude in degrees
+
+#     Returns
+#     -------
+#     east, north, up
+#         The east, north, up coordinates in meters
+#     """
+
+#     # Create topocentric proj obj based on specified origin
+#     topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
+
+#     # Create the projection transform pipeline string
+#     # This pipeline is for lonlatalt -> xyz -> enu
+#     projs = ["+proj=pipeline", ECEF_PROJ, topo_proj]
+#     pyproj_pipe = [proj.srs if not isinstance(proj, str) else proj for proj in projs]
+#     pyproj_pipeline = " +step ".join(pyproj_pipe)
+
+#     # Get transformer from pipeline string
+#     transformer = pyproj.Transformer.from_pipeline(pyproj_pipeline)
+
+#     return transformer.transform(lon, lat, alt, radians=False)
+
+
+# def enu2geodetic(
+#     east: float, north: float, up: float, lon_org: float, lat_org: float, alt_org: float
+# ) -> Tuple[float, float, float]:
+#     """
+#     Convert Topocentric coordinates (east,north,up) to Geodetic coordinates (lon,lat,alt)
+#     based on a specific topocentric origin described as geographic coordinates.
+
+#     See https://proj.org/operations/conversions/topocentric.html for more details.
+
+#     Parameters
+#     ----------
+#     east : float
+#         East in meters
+#     north : float
+#         North in meters
+#     up : float
+#         Up in meters
+#     lon_org : float
+#         Topocentric origin (x) as longitude in degrees
+#     lat_org : float
+#         Topocentric origin (y) as latitude in degrees
+#     alt_org : float
+#         Topocentric origin (z) as altitude in degrees
+
+#     Returns
+#     -------
+#     longitude, latitude, altitude
+#         The lon, lat, alt coordinates in degrees
+#     """
+
+#     # Create topocentric proj obj based on specified origin
+#     topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
+
+#     # Create projection transform pipeline string
+#     # This pipeline is for enu -> xyz
+#     pyproj_pipeline = f"+proj=pipeline +inv +step {topo_proj.srs}"
+
+#     # Get transformer from pipeline string
+#     transformer1 = pyproj.Transformer.from_pipeline(pyproj_pipeline)
+
+#     x, y, z = transformer1.transform(east, north, up, radians=False)
+
+#     # Return lonlatalt by converting xyz -> lonlatalt
+#     return geocentric2geodetic(x, y, z)
