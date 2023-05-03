bb8649d8426ae283d861f1d7c5cd84fd4b868419
Don Setiawan <landungs@uw.edu>
2023-05-03 09:48:03 -0700

    Add geo.py module for geospatial operations (#69)
    * Add geo.py module for geospatial operations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update geo module and add tests
    
    * Add __init__ for tests.fortran
    
    * Trying to debug fortran
    
    * Revert debug fortran
    
    * Add small fortran code and module prepare
    
    * Update plh2xyz
    
    * Round the results to 9 decimal digits
    
    * Remove rounding, already happened upstream
    
    * Update fortran program
    
    * Update permission to fortran program
    
    * Add platform print
    
    * Update comparison check and add multiple compiled version
    
    * Add type annotations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/seagap/utilities/geo.py
@@ -0,0 +1,267 @@
+"""geo.py
+
+Geospatial utilities module
+"""
+from typing import Tuple
+
+import numpy as np
+import pyproj
+
+ECEF_PROJ = pyproj.Proj(proj="geocent", ellps="WGS84")  # Earth Centered Fixed (x, y, z)
+LLA_PROJ = pyproj.Proj(
+    proj="longlat", ellps="WGS84"
+)  # Longitude Latitude (lon, lat, alt)
+GEODETIC_PRECISION = 9  # 9 decimal points
+GEOCENTRIC_PRECISION = 3  # 3 decimal points
+
+
+def geocentric2geodetic(x: float, y: float, z: float) -> Tuple[float, float, float]:
+    """
+    Converts Geocentric coordinate (x,y,z) to Geodetic coordinate (lon,lat,alt)
+    based on Ellipsoid `WGS84`
+
+    Parameters
+    ----------
+    x : float
+        Geocentric x in meters
+    y : float
+        Geocentric y in meters
+    z : float
+        Geocentric z in meters
+
+    Returns
+    -------
+    longitude, latitude, altitude
+        The lon, lat, alt coordinates in degrees
+    """
+    transformer = pyproj.Transformer.from_proj(ECEF_PROJ, LLA_PROJ)
+    coordinates = transformer.transform(x, y, z, radians=False)
+    return np.round(coordinates, GEODETIC_PRECISION)
+
+
+def geodetic2geocentric(
+    lon: float, lat: float, alt: float
+) -> Tuple[float, float, float]:
+    """
+    Converts Geodetic coordinate (lon,lat,alt) to Geocentric coordinate (x,y,z)
+    based on Ellipsoid `WGS84`
+
+    Parameters
+    ----------
+    lon : float
+        Longitude in degrees
+    lat : float
+        Latitude in degrees
+    alt : float
+        Altitude in degrees
+
+    Returns
+    -------
+    x, y, z
+        The x, y, z coordinates in meters
+    """
+    transformer = pyproj.Transformer.from_proj(LLA_PROJ, ECEF_PROJ)
+    coordinates = transformer.transform(lon, lat, alt, radians=False)
+    return np.round(coordinates, GEOCENTRIC_PRECISION)
+
+
+def __get_rotation_matrix(
+    lat_org: float, lon_org: float, to_enu: bool = True
+) -> np.ndarray:
+    """Helper function for ECEF to ENU and vice versa"""
+    # Setup
+    cos_lat = np.cos(lat_org)
+    sin_lat = np.sin(lat_org)
+    cos_lon = np.cos(lon_org)
+    sin_lon = np.sin(lon_org)
+
+    if to_enu:
+        return np.array(
+            [
+                [-sin_lon, cos_lon, 0],
+                [-sin_lat * cos_lon, -sin_lat * sin_lon, cos_lat],
+                [cos_lat * cos_lon, cos_lat * sin_lon, sin_lat],
+            ]
+        )
+    return np.array(
+        [
+            [-sin_lon, -sin_lat * cos_lon, cos_lat * cos_lon],
+            [cos_lon, -sin_lat * sin_lon, cos_lat * sin_lon],
+            [0, cos_lat, sin_lat],
+        ]
+    )
+
+
+def geocentric2enu(
+    x: float, y: float, z: float, lon_org: float, lat_org: float, alt_org: float
+) -> Tuple[float, float, float]:
+    """
+    Transform Geocentric coordinate (x,y,z) to a local ENU coordinate(east, north, up)
+    based on a reference point in Geodetic coordinate (lon, lat, alt)
+
+    See https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#From_ECEF_to_ENU
+
+    Parameters
+    ----------
+    x : float
+        Geocentric x in meters
+    y : float
+        Geocentric y in meters
+    z : float
+        Geocentric z in meters
+    lon_org : float
+        Reference origin longitude in degrees
+    lat_org : float
+        Reference origin latitude in degrees
+    alt_org : float
+        Reference origin altitude in degrees
+
+    Returns
+    -------
+    east, north, up
+        The east, north, up coordinates in meters
+    """
+    origin_xyz = geodetic2geocentric(lon_org, lat_org, alt_org)
+    delta_xyz = np.column_stack([np.array([x, y, z]) - origin_xyz])  # D(3, 1)
+
+    # Rotation matrix R(3, 3)
+    R = __get_rotation_matrix(lat_org, lon_org)
+    return np.dot(R, delta_xyz)  # E(3, 1)
+
+
+def enu2geocentric(
+    e: float, n: float, u: float, lat_org: float, lon_org: float, alt_org: float
+) -> Tuple[float, float, float]:
+    """
+    Transform local ENU coordinate(east, north, up) to Geocentric coordinate (x,y,z)
+    based on a reference point in Geodetic coordinate (lon, lat, alt)
+
+    See https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#From_ENU_to_ECEF
+
+    Parameters
+    ----------
+    east : float
+        East in meters
+    north : float
+        North in meters
+    up : float
+        Up in meters
+    lon_org : float
+        Reference origin longitude in degrees
+    lat_org : float
+        Reference origin latitude in degrees
+    alt_org : float
+        Reference origin altitude in degrees
+
+    Returns
+    -------
+    x, y, z
+        The x, y, z coordinates in meters
+    """
+    origin_xyz = np.column_stack(
+        [geodetic2geocentric(lon_org, lat_org, alt_org)]
+    )  # O(3, 1)
+    enu = np.column_stack([np.array([e, n, u])])  # E(3, 1)
+    # Rotation matrix R(3, 3)
+    R = __get_rotation_matrix(lat_org, lon_org, to_enu=False)
+    return np.dot(R, enu) + origin_xyz  # X(3, 1)
+
+
+# 04/25/2023 Don Setiawan
+# Code below uses Proj... but it provides different result
+# need to investigate as to what's going on here and its approach compared to the fortran
+# The Proj method does take into account the ellipsoid
+#
+# def _get_topo_proj(lon, lat, alt) -> pyproj.Proj:
+#     """Get topocentric Proj object"""
+#     return pyproj.Proj(proj="topocentric", ellps="WGS84", lat_0=lat, lon_0=lon, h_0=alt)
+
+# def geodetic2enu(
+#     lon: float, lat: float, alt: float, lon_org: float, lat_org: float, alt_org: float
+# ) -> Tuple[float, float, float]:
+#     """
+#     Convert Geodetic coordinates (lon,lat,alt) to Topocentric coordinates (east,north,up)
+#     based on a specific topocentric origin described as geographic coordinates.
+
+#     See https://proj.org/operations/conversions/topocentric.html for more details.
+
+#     Parameters
+#     ----------
+#     lon : float
+#         Longitude in degrees
+#     lat : float
+#         Latitude in degrees
+#     alt : float
+#         Altitude in degrees
+#     lon_org : float
+#         Topocentric origin (x) as longitude in degrees
+#     lat_org : float
+#         Topocentric origin (y) as latitude in degrees
+#     alt_org : float
+#         Topocentric origin (z) as altitude in degrees
+
+#     Returns
+#     -------
+#     east, north, up
+#         The east, north, up coordinates in meters
+#     """
+
+#     # Create topocentric proj obj based on specified origin
+#     topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
+
+#     # Create the projection transform pipeline string
+#     # This pipeline is for lonlatalt -> xyz -> enu
+#     projs = ["+proj=pipeline", ECEF_PROJ, topo_proj]
+#     pyproj_pipe = [proj.srs if not isinstance(proj, str) else proj for proj in projs]
+#     pyproj_pipeline = " +step ".join(pyproj_pipe)
+
+#     # Get transformer from pipeline string
+#     transformer = pyproj.Transformer.from_pipeline(pyproj_pipeline)
+
+#     return transformer.transform(lon, lat, alt, radians=False)
+
+
+# def enu2geodetic(
+#     east: float, north: float, up: float, lon_org: float, lat_org: float, alt_org: float
+# ) -> Tuple[float, float, float]:
+#     """
+#     Convert Topocentric coordinates (east,north,up) to Geodetic coordinates (lon,lat,alt)
+#     based on a specific topocentric origin described as geographic coordinates.
+
+#     See https://proj.org/operations/conversions/topocentric.html for more details.
+
+#     Parameters
+#     ----------
+#     east : float
+#         East in meters
+#     north : float
+#         North in meters
+#     up : float
+#         Up in meters
+#     lon_org : float
+#         Topocentric origin (x) as longitude in degrees
+#     lat_org : float
+#         Topocentric origin (y) as latitude in degrees
+#     alt_org : float
+#         Topocentric origin (z) as altitude in degrees
+
+#     Returns
+#     -------
+#     longitude, latitude, altitude
+#         The lon, lat, alt coordinates in degrees
+#     """
+
+#     # Create topocentric proj obj based on specified origin
+#     topo_proj = _get_topo_proj(lon_org, lat_org, alt_org)
+
+#     # Create projection transform pipeline string
+#     # This pipeline is for enu -> xyz
+#     pyproj_pipeline = f"+proj=pipeline +inv +step {topo_proj.srs}"
+
+#     # Get transformer from pipeline string
+#     transformer1 = pyproj.Transformer.from_pipeline(pyproj_pipeline)
+
+#     x, y, z = transformer1.transform(east, north, up, radians=False)
+
+#     # Return lonlatalt by converting xyz -> lonlatalt
+#     return geocentric2geodetic(x, y, z)
---
+++ tests/utilities/test_geo.py
@@ -0,0 +1,124 @@
+import platform
+import re
+import subprocess
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from seagap.utilities.geo import (
+    enu2geocentric,
+    geocentric2enu,
+    geocentric2geodetic,
+    geodetic2geocentric,
+)
+
+from ..fortran import flib
+
+AE = 6378137
+RF = 298.257222101
+HERE = Path(__file__).parent.absolute()
+
+
+@pytest.fixture(
+    params=[
+        [
+            (45.302840001, -124.965792528, -27.447300516),
+            (-2575288.225, -3682570.227, 4511064.31),
+            (30.65833784516843, -48.5432379637279, 35.870278707225),
+        ],
+        [
+            (45.302842926, -124.965768018, -26.772966283),
+            (-2575286.789, -3682571.528, 4511065.018),
+            (28.738677758968027, -48.62310043527105, 36.62160296509438),
+        ],
+        [
+            (45.30284281, -124.965767059, -26.767161545),
+            (-2575286.735, -3682571.582, 4511065.013),
+            (28.66260099824154, -48.63079082770161, 36.618411673613465),
+        ],
+        [
+            (45.302842778, -124.965766917, -26.767907543),
+            (-2575286.727, -3682571.59, 4511065.01),
+            (28.65133036714429, -48.63249083166475, 36.61575030903799),
+        ],
+        [
+            (45.302849701, -124.965658148, -26.727371295),
+            (-2575279.439, -3682576.054, 4511065.58),
+            (20.54714122627723, -51.119870518143685, 37.84139327034435),
+        ],
+    ]
+)
+def coordinates(request):
+    """Coordinates for `lat lon lat` and their `x y z` equivalent"""
+    return request.param
+
+
+@pytest.fixture
+def array_center():
+    return (45.3023, -124.9656, 0.0)
+
+
+def test_geodetic2geocentric(coordinates):
+    """Test for geodetic to geocentric conversion, comparing to fortran code"""
+    (lat, lon, alt), (x, y, z), _ = coordinates
+    input_data = f"{AE} {RF}\n{lat} {lon} {alt}".encode("utf-8")
+    fortran_program = f"plh2xyz-{platform.machine().lower()}"
+    # Calls on fortran code
+    result = subprocess.run(
+        [str((HERE.parent / "fortran" / fortran_program).absolute())],
+        input=input_data,
+        capture_output=True,
+    )
+    # Parse the string exported from the fortran code
+    fx, fy, fz = np.round(
+        np.array(re.findall(r"(-?\d+.\d+)+", result.stdout.decode("utf-8"))).astype(
+            float
+        ),
+        3,
+    )  # round to 3 decimal places
+
+    # Calls on the python code
+    px, py, pz = geodetic2geocentric(lon, lat, alt)
+
+    assert (fx, fy, fz) == (x, y, z)
+    assert (px, py, pz) == (x, y, z)
+
+    # Compare fortran and python values
+    assert (fx, fy, fz) == (px, py, pz)
+
+
+def test_geocentric2geodetic(coordinates):
+    """Test for geocentric to geodetic conversion"""
+    (lat, lon, alt), (x, y, z), _ = coordinates
+
+    plon, plat, palt = geocentric2geodetic(x, y, z)
+
+    assert np.allclose(
+        np.array((plat, plon, palt)), np.array((lat, lon, alt)), rtol=1e-10, atol=1e-10
+    )
+
+
+def test_geocentric2enu(coordinates, array_center):
+    _, (x, y, z), _ = coordinates
+    olat, olon, oalt = array_center
+    # Python enu
+    penu = geocentric2enu(x, y, z, olon, olat, oalt)
+
+    # Find delta array first
+    origin_array = geodetic2geocentric(olon, olat, oalt)
+    delta_array = np.column_stack([np.array([x, y, z]) - origin_array])
+
+    # Calculate enu with fortran lib
+    fenu = flib.xyz2enu(olat, olon, delta_array)
+    assert np.array_equal(np.round(fenu, 9), np.round(penu, 9))
+
+
+def test_enu2geocentric(coordinates, array_center):
+    _, (x, y, z), (e, n, u) = coordinates
+    olat, olon, oalt = array_center
+
+    pxyz = enu2geocentric(e, n, u, olat, olon, oalt)
+    expected = np.column_stack([np.array([x, y, z])])
+
+    assert np.array_equal(pxyz, expected)
