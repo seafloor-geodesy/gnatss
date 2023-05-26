3ef2c7cb2b4ac65350dcfb3d05dcc8b034313a4e
Don Setiawan <landungs@uw.edu>
2023-05-25 17:03:12 -0700

    Initialize ops module with necessary functions (#80)
    * Initialize ops module with necessary functions
    
    * Add some docstring and test for calc_std_and_verify
    
    * Add small test for find_gps_record
    
    * Add docstring and test for compute_enu_series
    
    * Add docstring for calc_uv and test
    
    * Add docstring and test for clean_zeros
    
    * Add default Q matrix
    
    * Add test for calculating two way travel time model
    
    * Add test for calc_tt_residual
    
    * Add docstring and test for calc_partials
    
    * Add docstring and test for calc_weight_matrix
    
    * Add docstring and test for calc_lsq, also move test data
    
    * Test calc_lsq directly from ATWA and ATWF
    
    * Add testing tolerance
    
    * Set atol=1e-5
    
    * Set atol=1e-4
    
    * Modify data and test for calc_lsq
    
    * Update messaging for clean_zeros and err type
    
    * Add constant default vector norm arr
    
    * Add check columns util func and test

---
+++ src/gnatss/ops.py
@@ -0,0 +1,444 @@
+from typing import Any, List, Tuple
+
+import numpy as np
+import pandas as pd
+import scipy
+from nptyping import Float64, NDArray, Shape
+
+from .configs.solver import ArrayCenter
+from .constants import (
+    GPS_COV_DIAG,
+    GPS_GEOCENTRIC,
+    GPS_GEODETIC,
+    GPS_LOCAL_TANGENT,
+    GPS_TIME,
+)
+from .utilities.geo import geocentric2enu, geocentric2geodetic
+
+# Constrain matrix Q
+Q_MATRIX = np.array(
+    [
+        [-1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    ]
+)
+
+DEFAULT_VECTOR_NORM = np.array([2.0, 0.0, 0.0])
+
+
+def _check_cols_in_series(input_series: pd.Series, columns: List[str]) -> None:
+    """Private func to check if the columns exists in data series"""
+    for col in columns:
+        if col not in input_series:
+            # Catch if some of thecolumns are missing
+            raise KeyError(f"``{col}`` not found in the data series provided.")
+
+
+def find_gps_record(
+    gps_solutions: pd.DataFrame, travel_time: pd.Timestamp
+) -> pd.Series:
+    """
+    Finds matching GPS record based on travel time
+    """
+    # TODO: Probably will change this function to perform some merging
+    match = gps_solutions.iloc[
+        gps_solutions[GPS_TIME]
+        .apply(lambda row: (row - travel_time))
+        .abs()
+        .argsort()[0],
+        :,
+    ]
+
+    return match
+
+
+def calc_std_and_verify(
+    gps_series: pd.Series, std_dev: bool = True, sigma_limit: float = 0.05
+) -> float:
+    """
+    Calculate the 3d standard deviation and verify the value based on limit
+
+    Parameters
+    ----------
+    gps_series : pd.Series
+        The data input to check as a pandas series.
+        The following keys are expected: 'xx', 'yy', 'zz'
+    std_dev : bool
+        Flag to indicate if the inputs are standard deviation or variance
+    sigma_limit : float
+        The allowable sigma limit to check against
+
+    Returns
+    -------
+    float
+        The calculated sigma 3d
+
+    Raises
+    ------
+    ValueError
+        If 3D Standard Deviation exceeds the GPS Sigma limit
+    """
+    # Checks for GPS Covariance Diagonal values
+    _check_cols_in_series(input_series=gps_series, columns=GPS_COV_DIAG)
+
+    # Compute the 3d std (sum variances of GPS components and take sqrt)
+    sig_3d = np.sqrt(np.sum(gps_series[GPS_COV_DIAG] ** (2 if std_dev else 1)))
+
+    # Verify sigma value, throw error if greater than gps sigma limit
+    if sig_3d > sigma_limit:
+        raise ValueError(
+            f"3D Standard Deviation of {sig_3d} exceeds GPS Sigma Limit of {sigma_limit}!"
+        )
+
+    return sig_3d
+
+
+def compute_enu_series(input_series: pd.Series, array_center: ArrayCenter) -> pd.Series:
+    """
+    Computes the longitude, latitude, and altitude values as well as the
+    east, north, up from ECEF (Geocentric) coordinates and add to the
+    input data series.
+
+    Parameters
+    ----------
+    input_series : pd.Series
+        The pandas data series that includes the x, y, z coordinates values
+    array_center : ArrayCenter
+        The array center object to be used for east, north, up calculation
+        as the origin coordinates
+
+    Returns
+    -------
+    pd.Series
+        A copy of the input data series with lon, lat, alt and east, north, up
+        columns added
+    """
+    # Catch if some of the coordinate columns are missing
+    _check_cols_in_series(input_series=input_series, columns=GPS_GEOCENTRIC)
+
+    if not isinstance(array_center, ArrayCenter):
+        # Catch if not ArrayCenter obj
+        raise ValueError(f"`array_center` input must be {type(ArrayCenter)} object")
+
+    array_center_coords = [array_center.lon, array_center.lat, array_center.alt]
+
+    location_series = input_series.copy()
+
+    geodetic_coords = geocentric2geodetic(*location_series[GPS_GEOCENTRIC].values)
+    enu_coords = geocentric2enu(
+        *location_series[GPS_GEOCENTRIC].values, *array_center_coords
+    ).flatten()
+
+    # Set geodetic lon,lat,alt to the series
+    for idx, v in enumerate(geodetic_coords):
+        location_series[GPS_GEODETIC[idx]] = v
+
+    # Set local tangent e,n,u to the series
+    for idx, v in enumerate(enu_coords):
+        location_series[GPS_LOCAL_TANGENT[idx]] = v
+
+    return location_series
+
+
+def calc_uv(input_vector: NDArray[Shape["3"], Any]) -> NDArray[Shape["3"], Any]:
+    """
+    Calculate unit vector for a 1-D input vector of size 3
+
+    Parameters
+    ----------
+    input_vector : (3,) ndarray
+        A 1-D input vector as numpy array
+
+    Returns
+    -------
+    (3,) ndarray
+        The resulting unit vector as numpy array
+
+    Raises
+    ------
+    ValueError
+        If the input vector is not a 1-D array
+    """
+
+    if input_vector.shape != (3,):
+        raise ValueError("Unit vector calculation must be 1-D array of shape 3!")
+
+    vector_norm = np.linalg.norm(input_vector)
+
+    if vector_norm == 0:
+        return DEFAULT_VECTOR_NORM
+
+    return input_vector / vector_norm
+
+
+def calc_twtt_model(
+    transmit_vectors: NDArray[Shape["3, *"], Float64],
+    reply_vectors: NDArray[Shape["3, *"], Float64],
+    transponders_mean_sv: NDArray[Shape["*"], Float64],
+) -> NDArray[Shape["*"], Float64]:
+    """
+    Calculate the Modeled TWTT (Two way travel time) in seconds
+
+
+    Parameters
+    ----------
+    transmit_vector : (3,N) ndarray
+        The transmit array of vectors
+    reply_vector : (3,N) ndarray
+        The reply array of vectors
+    transponders_mean_sv : (N,) ndarray
+        The transponders mean sound speed
+
+    Returns
+    -------
+    (N,) ndarray
+        The modeled two way travel times in seconds
+
+    """
+    # Calculate distances in meters
+    transmit_distance = np.array(
+        [np.linalg.norm(vector) for vector in transmit_vectors]
+    )
+    reply_distance = np.array([np.linalg.norm(vector) for vector in reply_vectors])
+
+    return (transmit_distance + reply_distance) / transponders_mean_sv
+
+
+def calc_tt_residual(
+    delays, transponder_delays, twtt_model
+) -> NDArray[Shape["*"], Float64]:
+    """
+    Calculate the travel time residual in seconds
+
+    Parameters
+    ----------
+    delays : (N,) ndarray
+        The measured travel time delays from data (sec)
+    transponder_delays : (N,) ndarray
+        The set transponder delays as defined in configuration file (sec)
+    twtt_model : (N,) ndarray
+        The modeled two way travel times (sec)
+
+    Returns
+    -------
+    (N,) ndarray
+        The travel time residual
+    """
+
+    # dA = Ameas - Amod
+    return (delays - transponder_delays) - twtt_model
+
+
+def calc_partials(
+    transmit_vectors: NDArray[Shape["3, *"], Float64],
+    reply_vectors: NDArray[Shape["3, *"], Float64],
+    transponders_mean_sv: NDArray[Shape["*"], Float64],
+    delays: NDArray[Shape["*"], Float64],
+    num_transponders: int,
+) -> Tuple[NDArray, NDArray, NDArray, NDArray]:
+    """
+    Calculate the partial derivative matrices
+
+    Parameters
+    ----------
+    transmit_vectors : (3,N) ndarray
+        The transmit array of vectors
+    reply_vectors : (3,N) ndarray
+        The reply array of vectors
+    transponders_mean_sv : (N,) ndarray
+        The transponders mean sound speed
+    delays : (N,) ndarray
+        The measured travel time delays from data (sec)
+    num_transponders : int
+        The total number of transponders
+
+    Returns
+    -------
+    A_partials : (num_transponders,num_transponders*3) ndarray
+        The A partial derivatives matrix
+    B_cov : (num_transponders,num_transponders*3) ndarray
+        The B covariance matrix
+    transmit_uv : (3,N) ndarray
+        The transmit array of unit vectors
+    reply_uv : (3,N) ndarray
+        The reply array of unit vectors
+    """
+
+    transmit_uv = np.array([calc_uv(v) for v in transmit_vectors])
+    reply_uv = np.array([calc_uv(v) for v in reply_vectors])
+
+    # Calculate the partial derivatives
+    partial_derivatives = (transmit_uv + reply_uv) / transponders_mean_sv
+    A_partials = np.zeros(shape=(num_transponders, num_transponders * 3))
+    B_cov = np.zeros(shape=(num_transponders, num_transponders * 3))
+
+    # computing non-differenced, direct ranging
+    # setup A partials and B covariance
+    for i in range(num_transponders):
+        if delays[i] > 0:
+            start_idx = i * num_transponders
+            end_idx = start_idx + 3  # Add 3 for x,y,z components
+            A_partials[i, start_idx:end_idx] = partial_derivatives[i]
+            B_cov[i, i] = 1.0
+
+    return A_partials, B_cov, transmit_uv, reply_uv
+
+
+def calc_weight_matrix(
+    transmit_uv: NDArray[Shape["3, *"], Float64],
+    gps_covariance_matrix: NDArray[Shape["3, 3"], Float64],
+    transponders_mean_sv: NDArray[Shape["*"], Float64],
+    b_cov: NDArray,
+    travel_times_variance: float,
+):
+    """
+    Calculate the weight matrix
+
+    Parameters
+    ----------
+    transmit_uv : (3,N) ndarray
+        The transmit array of unit vectors
+    gps_covariance_matrix : (3,3) ndarray
+        The covariance matrix from the gps data.
+        These are the values from "xx,xy,xz,yx,yy,yz,zx,zy,zz"
+    transponders_mean_sv : (N,) ndarray
+        The transponders mean sound speed
+    b_cov : (num_transponders,num_transponders*3) ndarray
+        The B covariance matrix
+    travel_times_variance : float
+        The user specified travel time variance value
+
+    Returns
+    -------
+    (num_transponders,num_transponders) ndarray
+        The resulting weight matrix
+    """
+    # Calculate covariance matrix for partlp vectors (COVF) Units m^2
+    covariance_matrix = np.abs((transmit_uv @ gps_covariance_matrix @ transmit_uv.T))
+
+    # Weighting factor equal to 2/sv_mean
+    weight_factor = 2.0 / (transponders_mean_sv**2)
+
+    # Convert to covariance of acoustic delays Units seconds^2
+    covariance_matrix = weight_factor * covariance_matrix
+
+    # Add travel times variance
+    np.fill_diagonal(
+        covariance_matrix, covariance_matrix.diagonal() + travel_times_variance
+    )
+
+    # TODO: Compute ADSIG
+    # IF (ADELAY(I).GT.0.0D0)  THEN
+    #   ADSIG(NEP,I) = DSQRT(COVF(I,I))
+    # ELSE
+    #   ADSIG(NEP,I) = 0.0D0       ! Unequivocal data flag
+    # END IF
+
+    # Reshape B_cov to be the same with COVF
+    cm_shape = covariance_matrix.shape
+    b_cov = b_cov[: cm_shape[0], : cm_shape[1]]
+
+    # Compute COV_SD [COV_SD = B_COV * COVF * B_COV^T]
+    covariance_std = b_cov @ covariance_matrix @ b_cov.T
+
+    return np.linalg.inv(covariance_std)
+
+
+def clean_zeros(input_array: NDArray) -> NDArray:
+    """
+    Trim the leading and/or trailing zeros from a 1-D or 2-D arrays.
+
+    Parameters
+    ----------
+    input_array : (N,) ndarray or (N,N) ndarray
+
+    Returns
+    -------
+    ndarray
+        The resulting N-D array with leading or trailing zeroes trimmed
+
+    Raises
+    ------
+    NotImplementedError
+        If the ``input_array`` not a 1 or 2-D array
+    """
+    num_dims = len(input_array.shape)
+    if num_dims == 1:
+        # 1D array
+        return np.array(np.trim_zeros(input_array))
+    elif num_dims == 2:
+        # 2D array
+        return np.array(
+            [np.trim_zeros(arr) for arr in input_array if np.trim_zeros(arr).size > 0]
+        )
+
+    raise NotImplementedError(
+        f"Only 1 or 2-D arrays are supported, instead for {num_dims} dimensions"
+    )
+
+
+# TODO: Q is currently hardcoded... this might change with
+# different transponders number, need adjustment
+def calc_lsq_contrained(
+    ATWA: NDArray[Shape["*, *"], Float64],
+    ATWF: NDArray[Shape["*"], Float64],
+    Q: NDArray[Shape["*, *"], Float64] = Q_MATRIX,
+):
+    """
+    Performs least-squares estimation with linear constraints.
+    This function has been translated almost directly from ``lscd2.f`` code.
+
+    Parameters
+    ----------
+    ATWA : ndarray
+        ATWA matrix from (A partials matrix)^T * Weight matrix * A partials matrix
+    ATWF : ndarray
+        ATWF matrix from (A partials matrix)^T * Weight matrix * Travel time residuals
+    Q : ndarray, optional
+      The Q constraint matrix
+
+    Returns
+    -------
+    X : (N,) ndarray
+        Solution vector without constraints
+    XP : (N,) ndarray
+        Solution vector with constraints
+    MX : (N,N) ndarray
+        Covariance matrix of solution without constraints
+    MXP : (N,N) ndarray
+        Covariance matrix of solution with constraints
+    """
+    # Unconstrained
+    X = scipy.optimize.lsq_linear(ATWA, ATWF).x
+
+    # Constrained
+    Zerr = 0 - (Q @ X)
+    Zerr = clean_zeros(Zerr)
+
+    MX = np.linalg.inv(ATWA)
+
+    MXQT = MX @ Q.T
+    MXQT = clean_zeros(MXQT)
+
+    QMXQT = Q @ MXQT
+    QMXQT = clean_zeros(QMXQT)
+
+    QMX = Q @ MX
+    QMX = clean_zeros(QMX)
+
+    QMXQTIQMX = np.linalg.inv(QMXQT) @ QMX
+
+    XDEL = clean_zeros(Zerr).T @ QMXQTIQMX
+    XP = X + XDEL
+
+    DELMX = MXQT @ QMXQTIQMX
+    MXP = MX - DELMX
+
+    return X, XP, MX, MXP
---
+++ tests/test_ops.py
@@ -0,0 +1,300 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from gnatss.configs.solver import ArrayCenter
+from gnatss.constants import (
+    GPS_COV,
+    GPS_GEOCENTRIC,
+    GPS_GEODETIC,
+    GPS_LOCAL_TANGENT,
+    GPS_TIME,
+)
+from gnatss.ops import (
+    DEFAULT_VECTOR_NORM,
+    _check_cols_in_series,
+    calc_lsq_contrained,
+    calc_partials,
+    calc_std_and_verify,
+    calc_tt_residual,
+    calc_twtt_model,
+    calc_uv,
+    calc_weight_matrix,
+    clean_zeros,
+    compute_enu_series,
+    find_gps_record,
+)
+
+from .ops_data import (
+    A_PARTIALS,
+    ATWA_SAMPLE,
+    ATWF_SAMPLE,
+    GPS_DATASET,
+    LSQ_RESULT,
+    REPLY_VECTORS,
+    TRANSMIT_VECTORS,
+    TRAVEL_TIMES_DATASET,
+    TT_DELAY_SECONDS,
+    TT_RESIDUAL,
+    TWTT_MODEL,
+    WEIGHT_MATRIX,
+)
+
+# TODO: Figure out how to test with multiple number of transponders data
+
+
+@pytest.fixture(
+    params=[
+        ArrayCenter(lat=45.3023, lon=-124.9656, alt=0.0),
+        dict(lat=45.3023, lon=-124.9656, alt=0.0),
+    ]
+)
+def array_center(request):
+    return request.param
+
+
+@pytest.fixture(params=GPS_DATASET)
+def gps_dataseries(request) -> pd.Series:
+    return pd.Series(request.param)
+
+
+@pytest.fixture(params=TRAVEL_TIMES_DATASET)
+def travel_time(request) -> float:
+    return request.param
+
+
+@pytest.fixture(params=[0.05, 0.001])
+def gps_sigma_limit(request):
+    return request.param
+
+
+@pytest.fixture
+def mean_sv():
+    return np.array([1481.542, 1481.513, 1481.5])
+
+
+@pytest.fixture
+def transponders_delay():
+    return np.array([0.2, 0.32, 0.44])
+
+
+@pytest.fixture
+def b_cov():
+    return np.array(
+        [
+            [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+
+@pytest.fixture
+def num_transponders():
+    return 3
+
+
+@pytest.fixture
+def travel_times_variance():
+    return 1e-10
+
+
+@pytest.mark.parametrize(
+    "input_data",
+    {
+        "c": 1,
+        "d": 2,
+        "e": 3,
+        "f": 4,
+        "g": 5,
+        "a": 6,
+        "b": 7,
+    },
+)
+@pytest.mark.parametrize("check_columns", [["a", "b", "c"], ["e", "z"], ["x"]])
+def test__check_cols_in_series(input_data, check_columns):
+    try:
+        input_series = pd.Series(input_data)
+        _check_cols_in_series(input_series=input_series, columns=check_columns)
+    except Exception as e:
+        if ("z" in check_columns) or ("x" in check_columns):
+            assert isinstance(e, KeyError)
+
+
+def test_find_gps_record(travel_time):
+    gps_df = pd.DataFrame.from_records(GPS_DATASET)
+
+    gps_ds = find_gps_record(gps_solutions=gps_df, travel_time=travel_time)
+
+    assert isinstance(gps_ds, pd.Series)
+
+
+def test_calc_std_and_verify(gps_dataseries, gps_sigma_limit):
+    """Tests the ``calc_std_and_verify`` function"""
+    try:
+        sig_3d = calc_std_and_verify(
+            gps_series=gps_dataseries, sigma_limit=gps_sigma_limit
+        )
+
+        assert isinstance(sig_3d, float)
+    except Exception as e:
+        if "yy" not in gps_dataseries:
+            # One of the dataseries doesn't have yy
+            # so Key error is expected
+            assert isinstance(e, KeyError)
+        elif gps_sigma_limit == 0.001:
+            # This is a really small sigma limit
+            # it will fail
+            assert isinstance(e, ValueError)
+
+
+def test_compute_enu_series(gps_dataseries, array_center):
+    """Tests the ``compute_enu_series`` function"""
+    location_columns = [GPS_TIME, *GPS_GEOCENTRIC]
+    try:
+        enu_series = compute_enu_series(
+            input_series=gps_dataseries, array_center=array_center
+        )
+        expected_columns = [*location_columns, *GPS_GEODETIC, *GPS_LOCAL_TANGENT]
+
+        assert all(col in enu_series for col in expected_columns)
+    except Exception as e:
+        if "z" not in gps_dataseries:
+            assert isinstance(e, KeyError)
+        elif isinstance(array_center, dict):
+            assert isinstance(e, ValueError)
+
+
+@pytest.mark.parametrize(
+    "input_vector,expected",
+    [
+        (np.array([0, 0, 0]), DEFAULT_VECTOR_NORM),
+        (np.array([1, 1, 1]), np.array([0.57735027, 0.57735027, 0.57735027])),
+        (np.array([-1, -2, 2]), np.array([-0.33333333, -0.66666667, 0.66666667])),
+        (np.array([[-1, 1, 3], [1, 1, 4]]), ValueError),
+        (np.array([-1, 2]), ValueError),
+    ],
+)
+def test_calc_uv(input_vector, expected):
+    """Test calculating unit vector"""
+    try:
+        unit_vector = calc_uv(input_vector=input_vector)
+        assert np.allclose(unit_vector, expected)
+    except Exception as e:
+        assert isinstance(e, expected)
+
+
+@pytest.mark.parametrize(
+    "transmit_vectors,reply_vectors,expected",
+    list(zip(TRANSMIT_VECTORS, REPLY_VECTORS, TWTT_MODEL)),
+)
+def test_calc_twtt_model(transmit_vectors, reply_vectors, expected, mean_sv):
+    """Test calculating two way travel time model"""
+    result = calc_twtt_model(
+        transmit_vectors=transmit_vectors,
+        reply_vectors=reply_vectors,
+        transponders_mean_sv=mean_sv,
+    )
+    assert np.allclose(result, expected)
+
+
+@pytest.mark.parametrize(
+    "tt_delay_seconds,twtt_model,expected_residual",
+    list(zip(TT_DELAY_SECONDS, TWTT_MODEL, TT_RESIDUAL)),
+)
+def test_calc_tt_residual(
+    tt_delay_seconds, twtt_model, expected_residual, transponders_delay
+):
+    """Test calculating travel time residual"""
+    res = calc_tt_residual(
+        delays=tt_delay_seconds,
+        transponder_delays=transponders_delay,
+        twtt_model=twtt_model,
+    )
+    assert np.allclose(res, expected_residual)
+
+
+@pytest.mark.parametrize(
+    "transmit_vectors,reply_vectors,tt_delays,expected_a",
+    list(zip(TRANSMIT_VECTORS, REPLY_VECTORS, TT_DELAY_SECONDS, A_PARTIALS)),
+)
+def test_calc_partials(
+    transmit_vectors,
+    reply_vectors,
+    tt_delays,
+    expected_a,
+    num_transponders,
+    mean_sv,
+    b_cov,
+):
+    A_partials, B_cov, _, _ = calc_partials(
+        transmit_vectors=transmit_vectors,
+        reply_vectors=reply_vectors,
+        transponders_mean_sv=mean_sv,
+        delays=tt_delays,
+        num_transponders=num_transponders,
+    )
+    assert np.allclose(A_partials, expected_a)
+    assert np.array_equal(B_cov, b_cov)
+
+
+@pytest.mark.parametrize(
+    "gps_dict,transmit_vectors,expected_wm",
+    list(zip(GPS_DATASET[2:], TRANSMIT_VECTORS, WEIGHT_MATRIX)),
+)
+def test_calc_weight_matrix(
+    gps_dict, transmit_vectors, expected_wm, mean_sv, b_cov, travel_times_variance
+):
+    gps_dataseries = pd.Series(gps_dict)
+    # Add missing data
+    if "z" not in gps_dataseries:
+        gps_dataseries["z"] = 4511065.769
+    if "yy" not in gps_dataseries:
+        gps_dataseries["yy"] = 0.001007144325
+
+    gps_covariance_matrix = np.array(np.array_split(gps_dataseries[GPS_COV], 3)).astype(
+        "float64"
+    )
+    transmit_uv = np.array([calc_uv(v) for v in transmit_vectors])
+    weight_matrix = calc_weight_matrix(
+        transmit_uv=transmit_uv,
+        gps_covariance_matrix=gps_covariance_matrix,
+        transponders_mean_sv=mean_sv,
+        b_cov=b_cov,
+        travel_times_variance=travel_times_variance,
+    )
+    assert np.allclose(weight_matrix, expected_wm)
+
+
+@pytest.mark.parametrize(
+    "input_array,expected",
+    [
+        # 2-D case
+        (
+            np.array([[1, 1, 1, 0, 0, 0], [2, 2, 2, 0, 0, 0], [3, 4, 5, 0, 0, 0]]),
+            np.array([[1, 1, 1], [2, 2, 2], [3, 4, 5]]),
+        ),
+        # 1-D case
+        (np.array([1, 3, 4, 0, 7, 8, 0, 0, 0]), np.array([1, 3, 4, 0, 7, 8])),
+        # 3-D case, fails
+        (np.ones(shape=(2, 2, 3)), NotImplementedError),
+    ],
+)
+def test_clean_zeros(input_array, expected):
+    try:
+        result = clean_zeros(input_array=input_array)
+        assert np.array_equal(result, expected)
+    except Exception as e:
+        assert isinstance(e, expected)
+
+
+def test_calc_lsq_contrained():
+    X, XP, MX, MXP = calc_lsq_contrained(ATWA_SAMPLE, ATWF_SAMPLE)
+
+    # The results are in meters and we'd like to ensure cm
+    # precision, so 1e-4 should be very precise,
+    # might even be overkill
+    assert np.allclose(X, LSQ_RESULT[0], atol=1e-4)
+    assert np.allclose(XP, LSQ_RESULT[1], atol=1e-4)
+    assert np.allclose(MX, LSQ_RESULT[2], atol=1e-4)
+    assert np.allclose(MXP, LSQ_RESULT[3], atol=1e-4)
