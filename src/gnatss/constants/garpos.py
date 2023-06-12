763bdc2483258f84e2d7a683bfbd2192f75cf776
Landung 'Don' Setiawan <landungs@uw.edu>
2023-06-12 14:58:10 -0700

    Expand constants module and add garpos consts

---
+++ src/gnatss/constants/garpos.py
@@ -0,0 +1,9 @@
+"""garpos.py
+
+Module for storing garpos constants
+"""
+
+ST = "ST"  # Transmission time of acoustic signal
+RT = "RT"  # Reception time of acoustic signal
+MT = "MT"  # ID of mirror transponder (should be consistent with Site-parameter file)
+TT = "TT"  # Observed travel time
