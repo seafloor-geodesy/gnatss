3e4a4f1fcd18ce940d7487185688830dd7212ddf
Landung 'Don' Setiawan <landungs@uw.edu>
2023-07-24 13:05:33 -0700

    Update and add tests for ops/data module

--- src/gnatss/ops/data.py
+++ src/gnatss/ops/data.py
@@ -1,9 +1,12 @@
 import numba
 import numpy as np
+import pandas as pd
+from nptyping import Float64, NDArray, Shape
 from numba.typed import List as NumbaList
 from pymap3d import ecef2enu, ecef2geodetic
 
 from .. import constants
+from ..configs.solver import ArrayCenter
 from .utils import _prep_col_names
 
 TRANSMIT_LOC_COLS = _prep_col_names(constants.GPS_GEOCENTRIC)
@@ -11,7 +14,25 @@ REPLY_LOC_COLS = _prep_col_names(constants.GPS_GEOCENTRIC, transmit=False)
 
 
 @numba.njit(cache=True)
-def _split_cov(cov_values):
+def _split_cov(
+    cov_values: NDArray[Shape["9"], Float64]
+) -> NDArray[Shape["3, 3"], Float64]:
+    """
+    Splits an array of covariance values of shape (9,)
+    to a matrix array of shape (3, 3)
+
+    Parameters
+    ----------
+    cov_values : (9,) ndarray
+        Covariance array of shape (9,)
+        in the order [xx, xy, xz,yx, yy, yx, yz, zx, zy, zz]
+
+    Returns
+    -------
+    (3,3) ndarray
+        The final covariance matrix of shape (3,3)
+    """
+
     n = 3
     cov = np.zeros((n, n))
     for i in range(n):
@@ -19,7 +40,24 @@ def _split_cov(cov_values):
     return cov
 
 
-def calc_lla_and_enu(all_observations, array_center):
+def calc_lla_and_enu(
+    all_observations: pd.DataFrame, array_center: ArrayCenter
+) -> pd.DataFrame:
+    """
+    Calculates the LLA and ENU coordinates for all observations
+
+    Parameters
+    ----------
+    all_observations : pd.DataFrame
+        The full dataset for computation
+    array_center : ArrayCenter
+        An object containing the center of the array
+
+    Returns
+    -------
+    pd.DataFrame
+        Modified dataset with LLA and ENU coordinates
+    """
     lla = all_observations[TRANSMIT_LOC_COLS].apply(
         lambda row: ecef2geodetic(*row.values), axis=1
     )
@@ -41,7 +79,21 @@ def calc_lla_and_enu(all_observations, array_center):
     return all_observations
 
 
-def get_data_inputs(all_observations):
+def get_data_inputs(all_observations: pd.DataFrame) -> NumbaList:
+    """Extracts data inputs to perform solving algorithm
+
+    Parameters
+    ----------
+    all_observations : pd.DataFrame
+        The full dataset for computation
+
+    Returns
+    -------
+    NumbaList
+        A list of data inputs
+    """
+    # Set up special numba list so it can be passed
+    # into numba functions for just in time compilation
     data_inputs = NumbaList()
 
     # Group obs by the transmit time
@@ -65,7 +117,7 @@ def get_data_inputs(all_observations):
     # Get transmit cov matrices
     cov_vals_df = grouped_obs[_prep_col_names(constants.GPS_COV, True)].first()
     gps_covariance_matrices = [
-        _split_cov(row.to_numpy()) for idx, row in cov_vals_df.iterrows()
+        _split_cov(row.to_numpy()) for _, row in cov_vals_df.iterrows()
     ]
 
     # Merge all inputs
