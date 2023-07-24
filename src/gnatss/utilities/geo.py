b2e57c64964a17852849a03dc72cd7415e3be07b
Landung 'Don' Setiawan <landungs@uw.edu>
2023-07-24 16:40:16 -0700

    Add docstring to geo

--- src/gnatss/utilities/geo.py
+++ src/gnatss/utilities/geo.py
@@ -9,7 +9,23 @@ from nptyping import Float64, NDArray, Shape
 def _get_rotation_matrix(
     lat_org: float, lon_org: float, to_enu: bool = True
 ) -> NDArray[Shape["3, 3"], Float64]:
-    """Helper function for ECEF to ENU and vice versa"""
+    """
+    Get rotation matrix for converting between ECEF and ENU
+
+    Parameters
+    ----------
+    lat_org : float
+        Origin latitude
+    lon_org : float
+        Origin longitude
+    to_enu : bool, default=True
+        If True, return matrix for converting from ECEF to ENU
+
+    Returns
+    -------
+    (3, 3) ndarray
+        Rotation matrix
+    """
     # Setup
     cos_lat = np.cos(lat_org)
     sin_lat = np.sin(lat_org)
