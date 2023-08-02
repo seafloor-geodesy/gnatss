71e224ffa55aecbc186f408e8ae9db5c133ede73
Madhav Mahesh Kashyap <madhavkashyap99@gmail.com>
2023-08-02 23:47:49 +0530

    * Add unix_j2000 to iso test cases verified on ported from fortan library.

--- tests/utilities/test_time.py
+++ tests/utilities/test_time.py
@@ -1,6 +1,8 @@
+import pytest
+
 from gnatss.utilities.time import AstroTime, erfa, isclose
 
-DATETIME_FORMAT = "%Y-%b-%dT%H:%M:%S.%f"
+DATETIME_FORMAT = "%Y-%b-%d %H:%M:%S.%f"
 
 
 def test_unix_j2000_time_format():
@@ -11,10 +13,10 @@ def test_unix_j2000_time_format():
 
     first_jan_2000_noon_unix_j2000 = AstroTime(0.0, format="unix_j2000")
     initial_unix_j2000_tt = AstroTime.strptime(
-        "2000-Jan-01T12:00:00.000", DATETIME_FORMAT, scale="tt"
+        "2000-Jan-01 12:00:00.000", DATETIME_FORMAT, scale="tt"
     )
     initial_unix_j2000_utc = AstroTime.strptime(
-        "2000-Jan-01T11:58:55.816", DATETIME_FORMAT, scale="utc"
+        "2000-Jan-01 11:58:55.816", DATETIME_FORMAT, scale="utc"
     )
     assert isclose(first_jan_2000_noon_unix_j2000.unix_j2000, 0.0, atol=1e-3)
     assert isclose(initial_unix_j2000_tt.unix_j2000, 0.0, atol=1e-3)
@@ -29,10 +31,10 @@ def test_unix_j2000_time_format():
         0.0 + secs_elapsed_till_second_feb_2004_noon, format="unix_j2000"
     )
     elapsed_unix_j2000_tt = AstroTime.strptime(
-        "2004-Feb-02T12:00:00.000", DATETIME_FORMAT, scale="tt"
+        "2004-Feb-02 12:00:00.000", DATETIME_FORMAT, scale="tt"
     )
     elapsed_unix_j2000_utc = AstroTime.strptime(
-        "2004-Feb-02T11:58:55.816", DATETIME_FORMAT, scale="utc"
+        "2004-Feb-02 11:58:55.816", DATETIME_FORMAT, scale="utc"
     )
     assert isclose(
         second_feb_2004_noon_unix_j2000.unix_j2000,
@@ -49,3 +51,21 @@ def test_unix_j2000_time_format():
         secs_elapsed_till_second_feb_2004_noon,
         atol=1e-3,
     )
+
+
+@pytest.mark.parametrize(
+    "unix_j2000, expected_iso",
+    [
+        (712215465.0, "2022-07-27 17:37:45.000"),
+        (712215465, "2022-07-27 17:37:45.000"),
+        ("712215465.0", "2022-07-27 17:37:45.000"),
+        (712215465, "2022-07-27 17:37:45.000"),
+        ("2022-07-27 17:37:45.000", None),
+    ],
+)
+def test_unix_j2000_convert_to_iso(unix_j2000, expected_iso):
+    if unix_j2000 == "2022-07-27 17:37:45.000":
+        with pytest.raises(ValueError):
+            AstroTime(unix_j2000, format="unix_j2000").iso
+    else:
+        assert AstroTime(unix_j2000, format="unix_j2000").iso == expected_iso
