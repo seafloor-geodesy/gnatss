bd627587cd3843512bcce5d2ca03d388deab0618
Madhav Mahesh Kashyap <madhavkashyap99@gmail.com>
2023-08-02 15:09:49 +0530

    Bug Fix Issue link: https://github.com/uw-ssec/offshore-geodesy/issues/108 * 'TimeUnixJ2000' now defines J2000 epoch as January 1, 2000, noon Terrestrial Time (previously used January 1, 2000, noon Terrestrial UTC).

--- tests/utilities/test_time.py
+++ tests/utilities/test_time.py
@@ -1,34 +1,51 @@
-from gnatss.utilities.time import AstroTime, erfa
+from gnatss.utilities.time import AstroTime, erfa, isclose
 
 DATETIME_FORMAT = "%Y-%b-%dT%H:%M:%S.%f"
 
 
 def test_unix_j2000_time_format():
-    """Tests custom time format unix_j2000"""
+    """
+    Tests custom time format unix_j2000.
+    Calculated times are verified with millisecond level tolerance.
+    """
 
     first_jan_2000_noon_unix_j2000 = AstroTime(0.0, format="unix_j2000")
-    first_jan_2000_noon_utc = AstroTime.strptime(
-        "2000-Jan-01T12:00:00.000", DATETIME_FORMAT, scale="utc"
+    initial_unix_j2000_tt = AstroTime.strptime(
+        "2000-Jan-01T12:00:00.000", DATETIME_FORMAT, scale="tt"
     )
-    assert (
-        first_jan_2000_noon_unix_j2000.unix_j2000
-        == first_jan_2000_noon_utc.unix_j2000
-        == 0.0
+    initial_unix_j2000_utc = AstroTime.strptime(
+        "2000-Jan-01T11:58:55.816", DATETIME_FORMAT, scale="utc"
     )
+    assert isclose(first_jan_2000_noon_unix_j2000.unix_j2000, 0.0, atol=1e-3)
+    assert isclose(initial_unix_j2000_tt.unix_j2000, 0.0, atol=1e-3)
+    assert isclose(initial_unix_j2000_utc.unix_j2000, 0.0, atol=1e-3)
 
-    # Number of seconds elapsed from 01-Jan-2000-noon-utc to 02_Feb-2004-noon-utc
+    # Number of seconds elapsed from 01-Jan-2000-noon-tt to 02_Feb-2004-noon-tt
     secs_elapsed_till_second_feb_2004_noon = float(
-        ((365 + 365 + 365 + 366) + 31 + 1) * erfa.DAYSEC
+        ((366 + 365 + 365 + 365) + 31 + 1) * erfa.DAYSEC
     )
 
     second_feb_2004_noon_unix_j2000 = AstroTime(
         0.0 + secs_elapsed_till_second_feb_2004_noon, format="unix_j2000"
     )
-    second_feb_2004_noon_utc = AstroTime.strptime(
-        "2004-Feb-02T12:00:00.000", DATETIME_FORMAT, scale="utc"
+    elapsed_unix_j2000_tt = AstroTime.strptime(
+        "2004-Feb-02T12:00:00.000", DATETIME_FORMAT, scale="tt"
     )
-    assert (
-        second_feb_2004_noon_unix_j2000.unix_j2000
-        == second_feb_2004_noon_utc.unix_j2000
-        == secs_elapsed_till_second_feb_2004_noon
+    elapsed_unix_j2000_utc = AstroTime.strptime(
+        "2004-Feb-02T11:58:55.816", DATETIME_FORMAT, scale="utc"
+    )
+    assert isclose(
+        second_feb_2004_noon_unix_j2000.unix_j2000,
+        secs_elapsed_till_second_feb_2004_noon,
+        atol=1e-3,
+    )
+    assert isclose(
+        elapsed_unix_j2000_tt.unix_j2000,
+        secs_elapsed_till_second_feb_2004_noon,
+        atol=1e-3,
+    )
+    assert isclose(
+        elapsed_unix_j2000_utc.unix_j2000,
+        secs_elapsed_till_second_feb_2004_noon,
+        atol=1e-3,
     )
