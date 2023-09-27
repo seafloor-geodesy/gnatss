f0d68ef9644dde9d2d596c7878518c22755e3118
Don Setiawan <landungs@uw.edu>
2023-09-27 13:21:06 -0700

    feat: Added azimuth and elevation calc (#156)
    * feat: Added azimuth and elevation calc
    
    Added 'ecef2ae' function to compute azimuth and elevation.
    Additionally, 'azimuth' and 'elevation' properties are now added
    to the 'SolverTransponder' object on instantiation.
    
    * feat: Add function to compute e,n,u components
    
    Added 'calc_enu_comp' to the 'geo' module for computing the
    mean 'e,n,u' components of residuals using the azimuth and elevation
    of the transponders
    
    * fix: Add value types for NDArray typehint

--- src/gnatss/utilities/geo.py
+++ src/gnatss/utilities/geo.py
@@ -2,8 +2,11 @@
 
 Geospatial utilities module
 """
+from typing import Tuple
+
 import numpy as np
 from nptyping import Float64, NDArray, Shape
+from pymap3d import Ellipsoid, ecef2enu
 
 
 def _get_rotation_matrix(
@@ -47,3 +50,103 @@ def _get_rotation_matrix(
             [0, cos_lat, sin_lat],
         ]
     )
+
+
+def ecef2ae(
+    x: float,
+    y: float,
+    z: float,
+    lat0: float,
+    lon0: float,
+    h0: float,
+    ell: Ellipsoid = None,
+    deg: bool = True,
+) -> Tuple[float, float]:
+    """
+    Compute azimuth and elevation from ECEF coordinates
+    w.r.t. a reference point
+
+    Parameters
+    ----------
+    x: float
+        target x ECEF coordinate (meters)
+    y: float
+        target y ECEF coordinate (meters)
+    z: float
+        target z ECEF coordinate (meters)
+    lat0: float
+        observer geodetic latitude
+    lon0: float
+        observer geodetic longitude
+    h0: float
+        observer altitude above geodetic ellipsoid (meters)
+    ell : Ellipsoid, optional
+        reference ellipsoid
+    deg : bool, optional
+        degrees input/output  (False: radians in/out)
+
+    Returns
+    -------
+    az: float
+        azimuth (degrees)
+    elev: float
+        elevation (degrees)
+    """
+    e, n, u = ecef2enu(x, y, z, lat0, lon0, h0, ell, deg=deg)
+
+    # 1 millimeter precision for singularity stability
+    try:
+        e[abs(e) < 1e-3] = 0.0
+        n[abs(n) < 1e-3] = 0.0
+        u[abs(u) < 1e-3] = 0.0
+    except TypeError:
+        if abs(e) < 1e-3:
+            e = 0.0
+        if abs(n) < 1e-3:
+            n = 0.0
+        if abs(u) < 1e-3:
+            u = 0.0
+
+    # Calculate hypotenuse
+    r = np.hypot(n, e)
+    elev = np.arctan(r / u)
+    az = np.arctan2(n, e)
+
+    if deg:
+        az = np.degrees(az)
+        elev = np.degrees(elev)
+    return az, elev
+
+
+def calc_enu_comp(
+    residuals: NDArray[Shape["*"], Float64],
+    az: NDArray[Shape["*"], Float64],
+    el: NDArray[Shape["*"], Float64],
+) -> NDArray[Shape["*"], Float64]:
+    """
+    Calculates the mean east, north, up components of the residuals
+    from N number of transponders.
+
+    Returns
+    -------
+    residuals : (N,) ndarray
+        Residuals values in centimeters (cm)
+    az : (N,) ndarray
+        Azimuth values in degrees (deg)
+    el : (N,) ndarray
+        Elevation values in degrees (deg)
+    """
+    # Perform checks
+    assert residuals.ndim == 1, "Residuals must be 1D"
+    assert az.ndim == 1, "Azimuth must be 1D"
+    assert el.ndim == 1, "Elevation must be 1D"
+    assert (
+        az.shape == el.shape == residuals.shape
+    ), "Azimuth, elevation, and residuals must have same shape"
+
+    # Compute north, east, and vertical components
+    res_north = np.mean(residuals * np.sin(np.radians(az)) * np.sin(np.radians(el)))
+    res_east = np.mean(residuals * np.cos(np.radians(az)) * np.cos(np.radians(el)))
+    res_vert = np.mean(residuals * np.cos(np.radians(el)))
+
+    return np.array([res_east, res_north, res_vert])
