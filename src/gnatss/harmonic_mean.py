20ec3fbd50ce4f530960e7fd18a4a623959ed18e
Landung 'Don' Setiawan <landungs@uw.edu>
2023-12-01 14:34:11 -0800

    feat: Add numba harmonic mean implementation
    Added '_sv_harmon_mean' numba implementation for
    calculating harmonic mean that uses calculations
    used within the original fortran code to get same
    harmonic values. Now there's a 'method' value for the
    main 'sv_harmonic_mean' function that allows user
    to switch back and forth b/w the two methods.
    
    ---
    
    Ref #193

--- src/gnatss/harmonic_mean.py
+++ src/gnatss/harmonic_mean.py
@@ -1,5 +1,10 @@
+from typing import Literal
+
+import numba
+import numpy as np
 import pandas as pd
 import scipy.stats
+from nptyping import Float64, NDArray, Shape
 
 from .constants import SP_DEPTH, SP_SOUND_SPEED
 
@@ -29,11 +34,7 @@ def _compute_hm(svdf: pd.DataFrame, start_depth: float, end_depth: float) -> flo
     end_depth : float
         The end depth for calculation
 
-    """  # noqa
-    for col in [SP_DEPTH, SP_SOUND_SPEED]:
-        if col not in svdf.columns:
-            raise ValueError(f"{col} column must exist in the input dataframe!")
-
+    """  # noqa: E501
     filtdf = svdf[
         (svdf[SP_DEPTH].round() >= start_depth) & (svdf[SP_DEPTH].round() <= end_depth)
     ]
@@ -44,7 +45,77 @@ def _compute_hm(svdf: pd.DataFrame, start_depth: float, end_depth: float) -> flo
     return scipy.stats.hmean(filtdf[SP_SOUND_SPEED], weights=weights, nan_policy="omit")
 
 
-def sv_harmonic_mean(svdf: pd.DataFrame, start_depth: float, end_depth: float) -> float:
+@numba.njit
+def _sv_harmon_mean(
+    dd: NDArray[Shape["*"], Float64],
+    sv: NDArray[Shape["*"], Float64],
+    zs: float,
+    ze: float,
+) -> float:
+    """
+    Compute harmonic mean of sound speed profile using algorithm from
+    the original fortran implementation.
+
+    Parameters
+    ----------
+    dd : (N,) ndarray
+        The depth data.
+        Values must be negative downward.
+    sv : (N,) ndarray
+        The sound speed data.
+        Values must be positive.
+    zs : float
+        The start depth for harmonic mean to be computed
+    ze : float
+        The end depth for harmonic mean to be computed
+
+    Returns
+    -------
+    float
+        The sound speed harmonic mean result
+    """
+    # Ensure that depth and sound speed arrays
+    # are the same shape
+    assert dd.shape == sv.shape
+
+    zi = zs
+    sum = 0.0
+
+    for i in range(0, dd.shape[0]):
+        z1, c_z1 = dd[i], sv[i]
+        z2, c_z2 = dd[i + 1], sv[i + 1]
+
+        if i > 0:
+            zi = z1
+        if z1 <= ze:
+            break
+
+        zf = z2
+        if z2 < ze:
+            zf = ze
+
+        # Compute slope
+        b = (c_z2 - c_z1) / (z2 - z1)
+
+        # Ensure that slope is not 0
+        assert b != 0.0
+
+        # Compute the weight
+        wi = np.log((zi - z1) * b + c_z1) / b
+        wf = np.log((zf - z1) * b + c_z1) / b
+        w_d = wf - wi
+        sum += w_d
+    svhm = (ze - zs) / sum
+
+    return svhm
+
+
+def sv_harmonic_mean(
+    svdf: pd.DataFrame,
+    start_depth: float,
+    end_depth: float,
+    method: Literal["scipy", "numba"] = "numba",
+) -> float:
     """
     Computes harmonic mean from a sound profile
     containing depth (dd) and sound speed (sv)
@@ -57,6 +128,13 @@ def sv_harmonic_mean(svdf: pd.DataFrame, start_depth: float, end_depth: float) -
         The start depth for harmonic mean to be computed
     end_depth : int or float
         The end depth for harmonic mean to be computed
+    method : {"scipy", "numba"}, default "numba"
+        The method to use for computing the harmonic mean.
+        The options are "scipy" and "numba".
+
+        "scipy" uses scipy.stats.hmean method.
+        "numba" uses numba.njit method and it's an equivalent algorithm,
+        to the original fortran implementation.
 
     Returns
     -------
@@ -67,9 +145,33 @@ def sv_harmonic_mean(svdf: pd.DataFrame, start_depth: float, end_depth: float) -
         raise ValueError("Dataframe is empty! Please check your data inputs.")
     # Clean up the sound speed value, ensuring that there's no negative value
     svdf = svdf[svdf[SP_SOUND_SPEED] > 0].reset_index(drop=True)
-    # Make all of the values absolute values, so we're only dealing with positives
-    abs_start = abs(start_depth)
-    abs_end = abs(end_depth)
-    abs_sv = abs(svdf)
 
-    return _compute_hm(abs_sv, abs_start, abs_end)
+    for col in [SP_DEPTH, SP_SOUND_SPEED]:
+        if col not in svdf.columns:
+            raise ValueError(f"{col} column must exist in the input dataframe!")
+
+    if method == "scipy":
+        # Make all of the values absolute values, so we're only dealing with positives
+        abs_start = abs(start_depth)
+        abs_end = abs(end_depth)
+        abs_sv = abs(svdf)
+
+        svhm = _compute_hm(abs_sv, abs_start, abs_end)
+    elif method == "numba":
+        # Ensure both start and end depths are negative
+        start_depth = -np.abs(start_depth)
+        end_depth = -np.abs(end_depth)
+
+        if start_depth < end_depth:
+            raise ValueError("Start depth must be greater than end depth!")
+
+        svdf = svdf[(svdf[SP_DEPTH].round() <= start_depth)]
+        # Extract the numpy arrays for depth and sound speed
+        # Make sure that the depth array has negative values
+        # and the sound speed array has positive values
+        dd = -np.abs(svdf[SP_DEPTH].values)
+        sv = np.abs(svdf[SP_SOUND_SPEED].values)
+
+        svhm = _sv_harmon_mean(dd, sv, start_depth, end_depth)
+
+    return svhm
