1e3b0876689a8227eb3ab694fa360bf77e50eae8
Madhav Mahesh Kashyap <madhavkashyap99@gmail.com>
2024-01-13 13:56:08 -0800

    (feat): Add atd_offset to posfilter config model * Linked to Issue #211 * Add AtdOffset pydantic model to handle antenna transducer offset config.

--- src/gnatss/configs/posfilter.py
+++ src/gnatss/configs/posfilter.py
@@ -9,6 +9,14 @@ from pydantic import BaseModel, Field
 from .io import InputData
 
 
+class AtdOffset(BaseModel):
+    """Antenna Transducer Offset base model."""
+
+    forward: float = Field(..., description="forward offset")
+    rightward: float = Field(..., description="rightward offset")
+    downward: float = Field(..., description="downward offset")
+
+
 class PositionFilterInputs(BaseModel):
     roll_pitch_heading: InputData = Field(
         ..., description="Roll Pitch Heading (RPH) data path specification."
@@ -23,3 +31,4 @@ class PositionFilter(BaseModel):
     input_files: PositionFilterInputs = Field(
         ..., description="Input files for position filtering routine."
     )
+    atd_offsets: AtdOffset = Field(..., description="Antenna Transducer Offset values.")
