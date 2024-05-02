55ee1a337530fcff0991ca043a6ccbaf961c59e7
Landung 'Don' Setiawan <landungs@uw.edu>
2024-05-02 16:42:27 -0700

    chore: Remove garpos and move constants to file

--- src/gnatss/constants/garpos.py
+++
@@ -1,9 +0,0 @@
-"""garpos.py
-
-Module for storing garpos constants
-"""
-
-ST = "T_transmit"  # Transmission time of acoustic signal
-RT = "T_receive"  # Reception time of acoustic signal
-MT = "MT_ID"  # ID of mirror transponder (should be consistent with Site-parameter file)
-TT = "TravelTime"  # Observed travel time
