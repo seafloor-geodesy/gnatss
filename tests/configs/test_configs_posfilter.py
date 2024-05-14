9f1b0e9335ae751ec4b05ed605cc604b308f8f53
Madhav <madhavkashyap99@gmail.com>
2024-05-13 19:22:45 -0700

    test: Fix Fix failing unittests caused by Configuration model changes * Fix tests/configs/test_configs_posfilter.py and tests/configs/test_configs_main.py

--- tests/configs/test_configs_posfilter.py
+++ tests/configs/test_configs_posfilter.py
@@ -17,23 +17,28 @@ def test_valid_position_filter_input(blank_csv_test_file: Path) -> None:
     # Test initialization of PositionFilter
     input_files = PositionFilterInputs(roll_pitch_heading=InputData(path=test_path))
     atdoffsets = AtdOffset(forward=0.0, rightward=-1.2, downward=3.2)
+
     pos_filter = PositionFilter(input_files=input_files, atd_offsets=atdoffsets)
     assert pos_filter.input_files.roll_pitch_heading.path == test_path
+    assert pos_filter.atd_offsets.forward == 0.0
+    assert pos_filter.atd_offsets.rightward == -1.2
+    assert pos_filter.atd_offsets.downward == 3.2
+
+    pos_filter = PositionFilter(atd_offsets=atdoffsets)
+    assert pos_filter.input_files is None
+    assert pos_filter.atd_offsets.forward == 0.0
+    assert pos_filter.atd_offsets.rightward == -1.2
+    assert pos_filter.atd_offsets.downward == 3.2
 
 
 def test_invalid_position_filter_input(blank_csv_test_file: Path) -> None:
     test_path = str(blank_csv_test_file)
     input_files = PositionFilterInputs(roll_pitch_heading=InputData(path=test_path))
-    atdoffsets = AtdOffset(forward=0.0, rightward=-1.2, downward=3.2)
 
     with pytest.raises(ValidationError) as e:
         _ = PositionFilter()
-    assert "2 validation error for PositionFilter\ninput_files\n" not in str(e)
+    assert "1 validation error for PositionFilter\natd_offsets\n" in str(e)
 
     with pytest.raises(ValidationError) as e:
         _ = PositionFilter(input_files=input_files)
     assert "1 validation error for PositionFilter\natd_offsets\n" in str(e)
-
-    with pytest.raises(ValidationError) as e:
-        _ = PositionFilter(atd_offsets=atdoffsets)
-    assert "1 validation error for PositionFilter\ninput_files\n" in str(e)
