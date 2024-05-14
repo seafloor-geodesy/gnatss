ced218e22c422dfba30304ae9c3a17cf495c4b8e
Madhav <madhavkashyap99@gmail.com>
2024-05-13 19:36:59 -0700

    * Revert deleted conftest lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
 import os
 from json import dumps
-from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Any, Dict, List
 
 import pytest
+from pandas import DataFrame
+
+from gnatss.configs.io import InputData
+from gnatss.configs.main import Configuration
+from gnatss.loaders import (
+    Path,
+    load_configuration,
+    load_novatel,
+    load_novatel_std,
+    load_roll_pitch_heading,
+    load_travel_times,
+)
+from gnatss.ops.io import gather_files_all_procs
+from tests import TEST_DATA_FOLDER
 
 
 @pytest.fixture
@@ -33,3 +47,58 @@ def blank_env(blank_csv_test_file: Path) -> None:
     # Clean up environment variables
     for k, v in blank_envs.items():
         os.environ.pop(k)
+
+
+@pytest.fixture(scope="session")
+def configuration() -> Configuration:
+    return load_configuration(TEST_DATA_FOLDER / "config.yaml")
+
+
+@pytest.fixture(scope="session")
+def all_files_dict() -> Dict[str, Any]:
+    config = load_configuration(TEST_DATA_FOLDER / "config.yaml")
+    return gather_files_all_procs(config)
+
+
+@pytest.fixture(scope="session")
+def novatel_data(all_files_dict) -> DataFrame:
+    data_files = all_files_dict["novatel"]
+    return load_novatel(data_files)
+
+
+@pytest.fixture(scope="session")
+def novatel_std_data(all_files_dict) -> DataFrame:
+    data_files = all_files_dict["novatel_std"]
+    return load_novatel_std(data_files)
+
+
+@pytest.fixture(scope="session")
+def transponder_ids(configuration) -> List[str]:
+    transponders = configuration.solver.transponders
+    return [t.pxp_id for t in transponders]
+
+
+@pytest.fixture(scope="session")
+def travel_times_data(all_files_dict, transponder_ids) -> DataFrame:
+    return load_travel_times(
+        files=all_files_dict["travel_times"],
+        transponder_ids=transponder_ids,
+        is_j2k=False,
+        time_scale="tt",
+    )
+
+
+@pytest.fixture(scope="session")
+def all_files_dict_roll_pitch_heading() -> Dict[str, Any]:
+    config = load_configuration(TEST_DATA_FOLDER / "config.yaml")
+    config.posfilter.input_files.roll_pitch_heading = InputData(
+        path="./tests/data/2022/NCL1/**/WG_*/RPH_TWTT",
+    )
+    return gather_files_all_procs(config)
+
+
+@pytest.fixture(scope="session")
+def roll_pitch_heading_data(all_files_dict_roll_pitch_heading) -> DataFrame:
+    return load_roll_pitch_heading(
+        all_files_dict_roll_pitch_heading["roll_pitch_heading"]
+    )
